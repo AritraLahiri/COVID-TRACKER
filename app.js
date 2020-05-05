e6d0ef5311bef891286a73db05737173e21c8549
AritraLahiri <aritralahiri17@gmail.com>
2020-05-05 15:26:51 +0530

    delpoiy

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const request = require('request');
 
-mongoose.connect(process.env.URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useFindAndModify: false
-});
+// mongoose.connect(process.env.URL, {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// 	useFindAndModify: false
+// });
+console.log(typeof process.env.URL);
 //
 
 //configs
